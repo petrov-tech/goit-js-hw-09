75d5582374517b4e56ad61e1d9cdbce8dc303845
petrov-tech <110338544+petrov-tech@users.noreply.github.com>
2023-01-14 18:58:20 +0000

    Deploying to gh-pages from @ petrov-tech/goit-js-hw-09@7564413cc932c8a014f53f5419af254a25201ddb 🚀

---
+++ 01-color-switcher.2381633d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.2381633d.js.map
